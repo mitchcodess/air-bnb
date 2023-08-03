307c91f944cdb960ec1f21c37e8bd4217d2cf6ed
Mitchell Hu <118404863+mitchcodess@users.noreply.github.com>
2023-08-03 15:24:07 -0400

    'Added session'

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,15 +4,24 @@ const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
+
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
 router.use(restoreUser);
 
-router.get(
-  '/restore-user',
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
+
 
 router.get('/set-token-cookie', async (_req, res) => {
   const user = await User.findOne({
@@ -24,6 +33,12 @@ router.get('/set-token-cookie', async (_req, res) => {
   return res.json({ user: user });
 });
 
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 router.get(
   '/require-auth',

04b61f14455072b4935f2c9b915b822a0518a2a2
Mitchell Hu <118404863+mitchcodess@users.noreply.github.com>
2023-08-09 22:46:03 -0400

    Modify Migration

--- backend/db/migrations/20230802150601-create-spot.js
+++ backend/db/migrations/20230802150601-create-spot.js
@@ -17,6 +17,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         references: {
           model: "Users",
+          foreignKey: 'userId'
         },
         onDelete: "CASCADE",
       },
